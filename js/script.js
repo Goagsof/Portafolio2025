62c82de2b1ec8826661edd3138208171f04f7de3
Goagsof <gagohm@gmail.com>
2025-03-19 18:23:11 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// script.js
+document.querySelector('.button:first-child').addEventListener('click', function() {
+  alert('Buscar con Google');
+});
+
+document.querySelector('.button:last-child').addEventListener('click', function() {
+  alert('Voy a tener suerte');
+});
